4d646d7ad74f71ce6c997edd9ab763d4a5564e49
mengtao <1481519672@qq.com>
2021-04-21 16:30:58 +0800

    Init

---
+++ src/main/java/com/Basic/DataType.java
@@ -0,0 +1,16 @@
+package com.Basic;
+
+public class DataType
+{
+    Integer id;
+
+    public DataType(Integer id)
+    {
+        this.id = id;
+    }
+
+    public Integer getId()
+    {
+        return id;
+    }
+}
---
+++ src/test/java/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import com.Basic.DataType;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        DataType dataType = new DataType(123);
+        System.out.printf("Hello world" + dataType.getId());
+    }
+}
